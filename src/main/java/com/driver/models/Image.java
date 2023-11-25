1bc81b0019f5d2b8045144ebee5672061691c38c
Rishabh Singh <rishabhjadon976@gmail.com>
2023-11-25 16:11:04 +0530

    second commit

--- src/main/java/com/driver/models/Image.java
+++ src/main/java/com/driver/models/Image.java
@@ -12,7 +12,7 @@ public class Image
 
     private String description;
 
-    private String dimension;
+    private String dimensions;
 
     @ManyToOne
     @JoinColumn
@@ -21,16 +21,16 @@ public class Image
     public Image() {
     }
 
-    public Image(int id, String description, String dimension) {
+    public Image(int id, String description, String dimensions) {
         this.id = id;
         this.description = description;
-        this.dimension = dimension;
+        this.dimensions = dimensions;
     }
 
-    public Image(int id, String description, String dimension, Blog blog) {
+    public Image(int id, String description, String dimensions, Blog blog) {
         this.id = id;
         this.description = description;
-        this.dimension = dimension;
+        this.dimensions = dimensions;
         this.blog = blog;
     }
 
@@ -50,12 +50,12 @@ public class Image
         this.description = description;
     }
 
-    public String getDimension() {
-        return dimension;
+    public String getDimensions() {
+        return dimensions;
     }
 
-    public void setDimension(String dimension) {
-        this.dimension = dimension;
+    public void setDimensions(String dimension) {
+        this.dimensions = dimensions;
     }
 
     public Blog getBlog() {
